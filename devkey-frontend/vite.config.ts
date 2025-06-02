fd5d4ac9c9c2ab12af841ca5605d997bf4975337
kaio-baleeiro <kaiobaleeiro@gmail.com>
2025-06-02 03:31:10 -0300

    chore(config): ajusta vite.config.ts para compatibilidade com Tailwind v3/v4

--- devkey-frontend/vite.config.ts
+++ devkey-frontend/vite.config.ts
@@ -1,7 +1,11 @@
+// devkey-frontend/vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import tailwindcss from '@tailwindcss/vite' // Importe o plugin do Vite
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(), // Adicione o plugin aqui
+  ],
 })
