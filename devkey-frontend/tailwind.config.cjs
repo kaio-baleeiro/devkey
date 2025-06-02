dbd198b26290379a986688c8ec84c544e2e6a2da
kaio-baleeiro <kaiobaleeiro@gmail.com>
2025-06-02 03:30:57 -0300

    chore(config): migra configuração do Tailwind para CommonJS (tailwind.config.cjs)

---
+++ devkey-frontend/tailwind.config.cjs
@@ -0,0 +1,12 @@
+// devkey-frontend/tailwind.config.cjs
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // Garanta que seus caminhos de conteúdo estão corretos
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
